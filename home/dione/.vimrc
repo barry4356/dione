dce86207af8cefc3bef5c5d86ed35b43273c2b5f
Barry <barryd@mail.usf.edu>
2023-05-20 11:15:46 -0400

    Pulled in config settings after first dry-run of samba server

---
+++ home/dione/.vimrc
@@ -0,0 +1,4 @@
+set tabstop=8 softtabstop=0
+set shiftwidth=2 smarttab
+set expandtab
+set mouse=a
---
+++ samba_config.sh
@@ -0,0 +1,11 @@
+
+sudo groupadd smbshare
+sudo umount /media/usb_mount 
+sudo mount /dev/sda1 /media/usb_mount/ -o dmask=000,fmask=111
+sudo chgrp -R smbshare /media/usb_mount #FAILED to changer mount permissions... still worked ok though
+sudo usermod -aG smbshare dione
+sudo smbpasswd -e dione
+sudo testparm # Test samba config
+sudo systemctl restart nmbd # restart samba server
+#sudo ufw allow from 192.168.205.0/24 to any app Samba # ufw firewall not enabled
+smbclient '\\localhost\public' -U dione #Test samba
