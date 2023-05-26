6cd8314e84a01d26324a9569c680c90fe53d335d
Barry <barryd@mail.usf.edu>
2023-05-25 21:21:06 -0400

    Adjust Raw Photo Sorting

--- perform_backup.sh
+++ perform_backup.sh
@@ -1,16 +1,22 @@
+# Setup Config Variables
 PRIMARY_DIRECTORY="/media/usb_mount/Photos"
 BACKUP_DIRECTORY="/media/usb_mount/Backup"
 LOGFILE="/media/usb_mount/Backup/log.txt"
 RAW_PRIMARY_DIR="${PRIMARY_DIRECTORY}/Raw"
-RAW_DAILY_DIR="${RAW_PRIMARY_DIR}/$(date +'%d%B%Y')"
 
+# Organize Raw Photos
 echo "$(date)" &>> ${LOGFILE}
 echo "Checking ${RAW_PRIMARY_DIR} For New Files..." &>> ${LOGFILE}
 for file in $(find ${RAW_PRIMARY_DIR} -maxdepth 1 -type f); do
-  echo "Moving [$file] to [${RAW_DAILY_DIR}]..." &>> ${LOGFILE}
-  mkdir -p ${RAW_DAILY_DIR}
-  mv $file ${RAW_DAILY_DIR}/.
+  DIRNAME=$(date -r ${file} +'%B%Y')
+  RAW_DATE_DIR="${RAW_PRIMARY_DIR}/$DIRNAME"
+  echo "Moving [$file] to [${RAW_DATE_DIR}]..." &>> ${LOGFILE}
+  mkdir -p ${RAW_DATE_DIR}
+  cp --backup=t $file ${RAW_DATE_DIR}/.
+  rm $file
 done
+
+# Sync to Backup
 echo "Sync-ing [${PRIMARY_DIRECTORY}] with [${BACKUP_DIRECTORY}]" &>> ${LOGFILE}
 rsync -rv ${PRIMARY_DIRECTORY} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
 echo "" &>> ${LOGFILE}
