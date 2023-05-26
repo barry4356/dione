161fe12effd39c14d102004f939f9b29f11058b6
Barry <barryd@mail.usf.edu>
2023-05-26 06:47:57 -0400

    Made script safer; added detail to log

--- perform_backup.sh
+++ perform_backup.sh
@@ -1,22 +1,28 @@
 # Setup Config Variables
-PRIMARY_DIRECTORY="/media/usb_mount/Photos"
+PHOTO_DIRECTORY="/media/usb_mount/Photos"
 BACKUP_DIRECTORY="/media/usb_mount/Backup"
 LOGFILE="/media/usb_mount/Backup/log.txt"
-RAW_PRIMARY_DIR="${PRIMARY_DIRECTORY}/Raw"
+RAW_PRIMARY_DIR="${PHOTO_DIRECTORY}/Raw"
 
 # Organize Raw Photos
 echo "$(date)" &>> ${LOGFILE}
-echo "Checking ${RAW_PRIMARY_DIR} For New Files..." &>> ${LOGFILE}
+echo "Checking Disk Usage..." &>> ${LOGFILE}
+df -h &>> ${LOGFILE}
+echo "----------------------" &>> ${LOGFILE}
+echo "Checking [${RAW_PRIMARY_DIR}] For New Files..." &>> ${LOGFILE}
 for file in $(find ${RAW_PRIMARY_DIR} -maxdepth 1 -type f); do
+  #TODO: Bug if the filename has a space in it...
   DIRNAME=$(date -r ${file} +'%B%Y')
   RAW_DATE_DIR="${RAW_PRIMARY_DIR}/$DIRNAME"
   echo "Moving [$file] to [${RAW_DATE_DIR}]..." &>> ${LOGFILE}
   mkdir -p ${RAW_DATE_DIR}
-  cp --backup=t $file ${RAW_DATE_DIR}/.
-  rm $file
+  cp --backup=t $file ${RAW_DATE_DIR}/. && rm $file
 done
 
-# Sync to Backup
-echo "Sync-ing [${PRIMARY_DIRECTORY}] with [${BACKUP_DIRECTORY}]" &>> ${LOGFILE}
-rsync -rv ${PRIMARY_DIRECTORY} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
+# Sync Photos to Backup
+echo "Sync-ing [${PHOTO_DIRECTORY}] with [${BACKUP_DIRECTORY}]" &>> ${LOGFILE}
+rsync -rv ${PHOTO_DIRECTORY} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
+echo "Checking Disk Usage..." &>> ${LOGFILE}
+df -h &>> ${LOGFILE}
+echo "----------------------" &>> ${LOGFILE}
 echo "" &>> ${LOGFILE}
