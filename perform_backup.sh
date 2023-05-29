0d39fb54ebfa7ab573f3a2788753035750f8fa7e
Barry <barryd@mail.usf.edu>
2023-05-29 19:53:14 -0400

    Update backup to include PC File Backups

--- perform_backup.sh
+++ perform_backup.sh
@@ -12,8 +12,8 @@ bash ${GIT_REPO}/check_temp.sh &>> ${LOGFILE}
 echo "----------------------" &>> ${LOGFILE}
 
 # Setup Photo-Backup Config Variables
-PHOTO_DIRECTORY="/media/usb_mount/Photos"
-BACKUP_DIRECTORY="/media/usb_mount/Backup"
+PHOTO_DIRECTORY="/mnt/primary_ext_drive/Photos"
+BACKUP_DIRECTORY="/mnt/secondary_ext_drive/Backup"
 RAW_PRIMARY_DIR="${PHOTO_DIRECTORY}/Raw"
 
 # Organize Raw Photos
@@ -24,17 +24,24 @@ for file in $(find ${RAW_PRIMARY_DIR} -maxdepth 1 -type f); do
   RAW_DATE_DIR="${RAW_PRIMARY_DIR}/$DIRNAME"
   echo "Moving [$file] to [${RAW_DATE_DIR}]..." &>> ${LOGFILE}
   mkdir -p ${RAW_DATE_DIR}
+  # mv -S .orig -b $file ${RAW_DATE_DIR}/.
   cp --backup=t $file ${RAW_DATE_DIR}/. && rm $file
 done
 
 # Sync Photos to Backup
 echo "Sync-ing [${PHOTO_DIRECTORY}] with [${BACKUP_DIRECTORY}]" &>> ${LOGFILE}
 rsync -rv ${PHOTO_DIRECTORY} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
+
+# Sync PC Backup to Secondary Drive
+PC_BACKUP_DIR="/mnt/primary_ext_drive/PCBackup"
+echo "Sync-ing [${PC_BACKUP_DIR}] with [${BACKUP_DIRECTORY}]" &>> ${LOGFILE}
+rsync -rv ${PC_BACKUP_DIR} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
+
+# Check Disk Usage
 echo "Checking Disk Usage..." &>> ${LOGFILE}
 df -h &>> ${LOGFILE}
 echo "----------------------" &>> ${LOGFILE}
 echo "" &>> ${LOGFILE}
-
 
 # Send Logfile to Git
 pushd $GIT_REPO &> /dev/null
