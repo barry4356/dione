950a4b70b7f8ddeabe8bc127da437c68ffc6f409
Barry <barryd@mail.usf.edu>
2023-06-17 19:21:02 -0400

    Ensure we preserve file change-times

--- scripts/perform_backup.sh
+++ scripts/perform_backup.sh
@@ -15,15 +15,15 @@ ${ROOT_DIR}/check_status.sh &>> ${LOGFILE}
 
 # Sync Photos to Backup
 echo "Sync-ing [${PHOTO_DIRECTORY}] with [${BACKUP_DIRECTORY}]" &>> ${LOGFILE}
-#rsync -rv --update ${PHOTO_DIRECTORY} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
+#rsync -rvt --update ${PHOTO_DIRECTORY} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
 
 # Sync PC Backup to Secondary Drive
 echo "Sync-ing [${PC_BACKUP_DIR}] with [${BACKUP_DIRECTORY}]" &>> ${LOGFILE}
-rsync -rv --update ${PC_BACKUP_DIR} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
+rsync -rvt --update ${PC_BACKUP_DIR} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
 
 # Sync Android Backup to Secondary Drive
 echo "Sync-ing [${ANDROID_BACKUP_DIR}] with [${BACKUP_DIRECTORY}]" &>> ${LOGFILE}
-rsync -rv --update ${ANDROID_BACKUP_DIR} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
+rsync -rvt --update ${ANDROID_BACKUP_DIR} ${BACKUP_DIRECTORY} --log-file=${LOGFILE}
 
 # Write System Status to Log
 ${ROOT_DIR}/check_status.sh &>> ${LOGFILE}
